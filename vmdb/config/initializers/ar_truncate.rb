1d68a42c27a52ffcfe5362207adcefc2c8745505
Joe Rafaniello <jrafanie@redhat.com>
2014-09-30 10:49:28 -0400

    Patch the existing connection object and any future connections.
    The first line of the initializer, `ActiveRecord::Base.connection.respond_to? :truncate`,
    instantiates a connection object so we have to extend that object in addition to
    any future connection objects.
    
    Before:
    irb(main):001:0> ActiveRecord::Base.connection.respond_to? :truncate
    => false
    
    After:
    irb(main):001:0> ActiveRecord::Base.connection.respond_to? :truncate
    => true

--- vmdb/config/initializers/ar_truncate.rb
+++ vmdb/config/initializers/ar_truncate.rb
@@ -1,9 +1,16 @@
 if ActiveRecord::Base.connection.respond_to? :truncate
   puts "Rails is new enough to have truncate, please remove this: #{__FILE__}"
 else
-  class << ActiveRecord::Base.connection.class
+  module ArTruncate
     def truncate(table_name, name = nil)
       execute("TRUNCATE TABLE #{quote_table_name(table_name)}", name)
     end
+  end
+
+  # Patch the already created connection object and any future ones
+  ActiveRecord::Base.connection.extend ArTruncate
+
+  class << ActiveRecord::Base.connection.class
+    include ArTruncate
   end
 end
